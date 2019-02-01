84255a263dbdd6f575f4e448617ce555f2740dd8
lj <lj@192.168.32.1>
2019-02-01 21:10:16 +0800

    添加部分页面

---
+++ src/main/java/com/brett/Entity/ImageUtil.java
@@ -0,0 +1,36 @@
+package com.brett.Entity;
+
+import java.io.*;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageUtil {
+	 /**
+     * 保存文件，直接以multipartFile形式
+     * @param multipartFile
+     * @param path 文件保存绝对路径
+     * @return 返回文件名
+     * @throws IOException
+     */ public static String saveImg(MultipartFile multipartFile,String path) throws IOException { 
+    	 File file = new File(path); 
+    	 if (!file.exists()) { 
+    		 file.mkdirs(); 
+    		 } 
+    	 FileInputStream fileInputStream = (FileInputStream) multipartFile.getInputStream(); 
+    	 String fileName = "" + ".png"; 
+    	 BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path + File.separator + fileName)); 
+    	 byte[] bs = new byte[1024]; 
+    	 int len;
+    	 while ((len = fileInputStream.read(bs)) != -1) { 
+    		 bos.write(bs, 0, len); 
+    		 } 
+    	 bos.flush(); 
+    	 bos.close(); 
+    	 return fileName; 
+    	 }
+
+	public static String getFilePath(String currentUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- src/main/java/com/brett/controller/RoomController.java
+++ src/main/java/com/brett/controller/RoomController.java
@@ -1,13 +1,58 @@
 package com.brett.controller;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.alibaba.fastjson.JSON;
+import com.brett.Entity.ImageUtil;
 
 @Controller
 public class RoomController {
+	
+	private static final Logger log = LoggerFactory.getLogger(RoomController.class);
+
 
 	@RequestMapping("/room")
 	public String room() {
 		return "coc/room";
 	}
+	
+	@PutMapping("/article/img/upload") 
+	public JSON uploadImg(@RequestParam("editormd-image-file") MultipartFile multipartFile) { 
+		if (multipartFile.isEmpty() || StringUtils.isEmpty(multipartFile.getOriginalFilename())) { 
+			
+			} 
+		String contentType = multipartFile.getContentType(); 
+		if (!contentType.contains("")) { 
+			
+			} 
+		String root_fileName = multipartFile.getOriginalFilename(); 
+		log.info("上传图片:name={},type={}", root_fileName, contentType); 
+		String location="";
+		//处理图片 User 
+		String currentUser = ""; 
+		//获取路径 String 
+		String return_path = ImageUtil.getFilePath(currentUser); 
+		String filePath = location + return_path; 
+		log.info("图片保存路径={}", filePath); 
+		String file_name = null; 
+		JSON json=new JSON() {};
+		try { 
+			file_name = ImageUtil.saveImg(multipartFile, filePath);
+			if(!StringUtils.isEmpty(file_name)){ 
+
+				} 
+			log.info("返回值：{}",json); 
+			} 
+		catch (IOException e) 
+		{ 
+		}
+		return json; 
+	}	
 }
